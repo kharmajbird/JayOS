1cd525c36bc2098fb113a228c84febabdf721f5d
James Lewis <kharmajbird@gmail.com>
2012-01-06 18:07:31 -0800

    added new usb directory to dl script

--- scripts/dl.sh
+++ scripts/dl.sh
@@ -8,7 +8,7 @@
 S3=http://jayosrc.s3-website-us-west-1.amazonaws.com/tarballs
 PKGLIST=/lfs/build/JayOS/figs/pkglist
 HEAD=/lfs/build/JayOS/tarballs
-DIRS="jlfs lfs"
+DIRS="jlfs lfs usb"
 XDIRS="xorg/app xorg/driver xorg/font xorg/lib xorg/proto xorg/util xorg/xserver"
 
 for i in $DIRS $XDIRS; do mkdir -p $HEAD/$i; done
